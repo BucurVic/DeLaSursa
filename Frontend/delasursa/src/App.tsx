8c11d66a3193d5342fb1e4998823cc4ccbae82d1
Husăsan Smaranda-Ioana <smara.husasan@gmail.com>
2025-11-11 00:41:16 +0200

    route for ProductsPage

--- Frontend/delasursa/src/App.tsx
+++ Frontend/delasursa/src/App.tsx
@@ -1,8 +1,14 @@
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 import "./App.css";
+import ProductsPage from "./pages/ProductsPage.tsx";
 
 function App() {
     return (
-        <></>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/products" element={<ProductsPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
