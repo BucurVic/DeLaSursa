6e2ab114828c284bc26ffc8410811115da563427
Husăsan Smaranda-Ioana <smara.husasan@gmail.com>
2025-12-11 03:20:18 +0200

    added image and measure unit for ComandaProdusDto

--- Backend/DeLaSursa/src/main/java/org/example/delasursa/common/dto/comanda/ComandaProdusDto.java
+++ Backend/DeLaSursa/src/main/java/org/example/delasursa/common/dto/comanda/ComandaProdusDto.java
@@ -1,6 +1,9 @@
 package org.example.delasursa.common.dto.comanda;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -25,5 +28,7 @@ public class ComandaProdusDto {
         String categorie;
         String numeProducator;
         Double pret;
+        String imagineProdus;
+        String unitateDeMasura;
     }
 }
